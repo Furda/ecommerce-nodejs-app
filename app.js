d842d07d70c30aa474ff84e195cfff8e129f2f47
Fernando <furdac14@gmail.com>
2020-06-15 14:39:29 -0400

    9. Dynamic Routes & Advanced Models

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
-// core Node.js modules
 const path = require('path');
 
-// third-party apps
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 
-// local files
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
-// set template engine
-app.set('view engine', 'ejs' );
+const app = express();
 
-// where to find the templates (path) (default is set to views folder)
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// body parser 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-// static files
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-// only can reach with /admin/... paths
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// page not found error
 app.use(errorController.get404);
 
-//listen for a port
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const path = require('path');
 
-const router = express.Router();
+const express = require('express');
 
 const adminController = require('../controllers/admin');
 
-// path: /admin/add-product
+const router = express.Router();
+
+// /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
 router.get('/products', adminController.getProducts);
 
-// path: /admin/product --> POST
+// /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
